be34cbc18becbec83ee80d5b864438bcd7475ac6
Edgar-PTC <20230280@ricaldone.edu.sv>
2025-06-24 14:00:27 -0600

    Ya sirve index

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-const API_URL = '685af76589952852c2d83324.mockapi.io/UShoes/shoes'; 
+const API_URL = 'https://685af76589952852c2d83324.mockapi.io/UShoes/shoes'; 
 
 const container = document.getElementById("Shoes-div");
 
@@ -26,9 +26,9 @@ function CargarTarjetas(zapatos){
             <div class="card">
                 <img src="${zapato.imagen}" alt="">
                 <h2>${zapato.nombre}</h2>
-                <div style="display: flex; justify-content: space-between;">
+                <div>
                     <h3>${zapato.categoria}</h3>
-                    <h4>${zapato.precio}</h4>               
+                    <h4>$${zapato.precio}</h4>               
                 </div>
                 <button class="card-button">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
